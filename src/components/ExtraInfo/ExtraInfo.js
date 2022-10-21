29c57e83e4aafdba1e74c9d3fe49dfeee761edb7
sohagzayan <gdsohag360@gmail.com>
2022-10-20 21:41:31 -0700

    re-stye filtter button also customie extra section

--- src/components/ExtraInfo/ExtraInfo.js
+++ src/components/ExtraInfo/ExtraInfo.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { HiOutlineLocationMarker } from "react-icons/hi";
+import { BsPencil } from "react-icons/bs";
 import { AiOutlineClose, AiOutlineInfoCircle } from "react-icons/ai";
 import "./ExtraInfo.css";
 
 const ExtraInfo = () => {
+  const [location, setLocation] = useState("");
   return (
     <div>
       <div>
         <form action="" className="search_location position-relative">
           <HiOutlineLocationMarker className="iconsLocation" />
-          <input type="text" placeholder="Enter your location" className="" />
-          <AiOutlineClose className="iconsClose" />
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            type="text"
+            value={location}
+            placeholder="Enter your location"
+            className=""
+          />
+          {location.length ? (
+            <AiOutlineClose
+              onClick={() => setLocation("")}
+              className="iconsClose"
+            />
+          ) : (
+            <BsPencil className="iconsClose" />
+          )}
         </form>
-        <p className="mt-4">
-          <AiOutlineInfoCircle className="fs-4 mr-4 d-inline-block" />
-          Your location will help us serve better and extend a personalised
-          experience.
+        <p className="mt-4 d-flex gap-2">
+          <span>
+            <AiOutlineInfoCircle className="fs-4 mr-4 d-inline-block" />
+          </span>
+          <span>
+            Your location will help us serve better and extend a personalised
+            experience.
+          </span>
         </p>
       </div>
     </div>
--- src/components/MobileGroupController/MobileGroupController.js
+++ src/components/MobileGroupController/MobileGroupController.js
@@ -1,13 +1,12 @@
 import React from "react";
-
+import "./MobileGroupController.css";
 const MobileGroupController = () => {
   return (
     <div className="mobileView d-lg-none d-block">
-      <div className="container d-flex justify-content-between align-items-center py-3">
+      <div className="container d-flex justify-content-between align-items-center py-3 ">
         <h6>Posts(368)</h6>
-
-        <div>
-          <span>Filter: All</span>
+        <div className="filterCategoryField">
+          <span>Filter:</span>
           <select name="" id="">
             <option value="all">All</option>
             <option value="article">Article</option>
